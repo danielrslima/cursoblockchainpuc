28209cb0516aad2551083540ddf1a44be83814bc
Daniel Lima <danielrslima@gmail.com>
2019-10-03 22:55:46 -0300

    Contrato Escrow
    Contrato Solange

---
+++ escrow.sol
@@ -0,0 +1,45 @@
+pragma solidity >=0.5.1;
+
+contract Escrow {
+    address payable public comprador; 
+    address payable public vendedor;
+    uint256 public preco;
+    bool public entregue;  
+    
+    constructor (address payable novo_comprador, address payable novo_vendedor, uint256 novo_preco) public {
+        comprador = novo_comprador;
+        vendedor = novo_vendedor;
+        preco = novo_preco;
+        entregue = false;
+    }
+
+    function pagamento () payable public {
+        //O produto não pode ter sido entregue ainda
+        require (!entregue, "Produto pago e entregue ");
+        
+        //Quem está enviando deve ser o comprador
+        require (comprador == msg.sender, "Quem esta tentando pagar nao é o comprador");
+        
+        //O valor enviado deve ser igual ao preço
+        require (msg.value == preco, "Valor diferente do preco");
+
+        //entrega produto para o comprador
+        entregue = true;
+                
+        //transfere valor para o vendedor
+        if (preco > 0) {
+            address(vendedor).transfer(msg.value);
+        }
+                
+    } 
+    
+    function definePartes (address payable _comprador, address payable _vendedor, uint256 _preco) public {
+        //Só pode reiniciar se o produto já foi entregue
+        require (entregue, "Em andamento, não é possível alterar"); 
+        
+        comprador = _comprador;
+        vendedor = _vendedor;
+        preco = _preco;
+        entregue = false;
+    }
+} 
