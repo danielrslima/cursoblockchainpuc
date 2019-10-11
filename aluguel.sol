6928d8a4e88b3a6123095255f87a29d12289e1ad
Daniel Lima <danielrslima@gmail.com>
2019-10-10 22:20:09 -0300

    Aluguel 5
    Acrescentado a função status pagamento

--- aluguel.sol
+++ aluguel.sol
@@ -19,30 +19,34 @@ constant = uint256 constant (tem sempre que informar o numero/valor)
     string public locador;
     uint256 private valor;
     uint256 constant numeroMaximoLegalDeAlugueisParaMulta = 3;
+    bool[] public statusPagamento;
+    address payable public contaLocatario;
     
     constructor( 
-        string memory nomeLocador, string memory nomeLocatario, uint256 valorDoAluguel) public {
+        string memory nomeLocador, string memory nomeLocatario, address payable paramContaLocatario, uint256 valorDoAluguel) public 
+    {
         locador = nomeLocador;
         locatario = nomeLocatario;
         valor = valorDoAluguel;
-        }
+        contaLocatario = paramContaLocatario;
+    }
 
 // "contructor" sempre tem que ser publico
-    function valorAtualDoAluguel() public view returns (uint256) {
+    function valorAtualDoAluguel() public view returns (uint256) 
+    {
         return valor;
     }
     
     function simulaMulta (uint256 mesesRestantes, uint256 totalMesesContrato)
-    public
-    view
-    returns(uint256 valorMulta) {
+    public view returns(uint256 valorMulta) {
         valorMulta = valor*numeroMaximoLegalDeAlugueisParaMulta;
         valorMulta = valorMulta/totalMesesContrato;
         valorMulta = valorMulta*mesesRestantes;
         return valorMulta;
     }
     
-    function reajustaAluguel(uint256 percentualReajuste) public {
+    function reajustaAluguel(uint256 percentualReajuste) public 
+    {
         if (percentualReajuste > 20)
         {
             percentualReajuste = 20;
@@ -56,11 +60,21 @@ constant = uint256 constant (tem sempre que informar o numero/valor)
         valor = valorCerto;
     }
     
-    function aplicaMulta (uint256 mesesRestantes, uint256 percentual) public{
-        require(mesesRestantes < 30, "Periodo de contrato invaido");
-        for (uint256 i=1; i<mesesRestantes; i++) {
+    function aplicaMulta (uint256 mesesRestantes, uint256 percentual) public
+    {
+        require(mesesRestantes <= 30, "Periodo de contrato invalido");
+        for (uint256 i=1; i<mesesRestantes; i++) 
+        {
             valor = valor+((valor*percentual)/100);
         }
+    }
+    
+    function EfetuarPagamento() public payable 
+    {
+        require(msg.value>=valor, "Valor Insuficiente");
+        contaLocatario.transfer(msg.value);
+        statusPagamento.push(true);
+        
     }    
 }    
  
