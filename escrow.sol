73d67b5ed6d670cb1230c71153de4993674f5001
Daniel Lima <danielrslima@gmail.com>
2019-10-31 14:27:24 -0300

    escrow.sol
    Ajustei function pagamento

--- escrow.sol
+++ escrow.sol
@@ -18,17 +18,18 @@ Poderia dizer que o mercadopago se assemelha a um escrow (pois assegura que se o
     }
 
     function pagamento () payable public {
-        //O produto não pode ter sido entregue ainda
-        require (!entregue, "Produto pago e entregue ");
+        //O produto pode não ter sido entregue ainda
+        require (!entregue, "O Produto foi pago e entregue ");
         
         //Quem está enviando deve ser o comprador
         require (comprador == msg.sender, "Quem esta tentando pagar nao é o comprador");
         
         //O valor enviado deve ser igual ao preço
-        require (msg.value == preco, "Valor diferente do preco");
+        require (msg.value == preco, "Valor está diferente do preco");
 
         //entrega produto para o comprador
         entregue = true;
+        
                 
         //transfere valor para o vendedor
         if (preco > 0) {
